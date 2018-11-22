f2f32230fd0db949375f1abda754b34293f215b5
Graham Barber <grahammbarber@gmail.com>
2018-11-21 20:57:21 -0800

    chore: set up frontend

---
+++ frontend/pages/about/index.js
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+import Link from 'next/link'
+import Header from '../../components/header'
+import propTypes from 'prop-types'
+
+class AboutPage extends Component {
+  static getInitialProps () {
+    const isServer = typeof window === 'undefined'
+    return { isServer }
+  }
+
+  render () {
+    return (
+      <main>
+        <Header />
+        <section>
+          <p>
+            This is another page of the SSR example, you accessed it{' '}
+            <strong>{this.props.isServer ? 'server' : 'client'} side</strong>.
+          </p>
+          <p>You can reload to see how the page change.</p>
+          <Link href='/'>
+            <a>Go to Home</a>
+          </Link>
+        </section>
+      </main>
+    )
+  }
+}
+
+AboutPage.propTypes = {
+  isServer: propTypes.bool
+}
+
+export default AboutPage
